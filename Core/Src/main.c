83a5851cf2955cf07abe8a967e0323fb9e2e6b87
Panagiotis Mantas <mantas.pan1@gmail.com>
2025-01-22 17:11:05 +0200

    Newversion

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -155,7 +155,7 @@ int main(void)
   check = send_at_command_and_check_response("AT+CPIN?\r\n", "READY", rx_buffer);
   check = send_at_command_and_check_response("AT+CGNSPWR?\r\n", "OK\r\n", rx_buffer);
   check = send_at_command_and_check_response("AT+CGNSPWR=1\r\n", "OK\r\n", rx_buffer);
-  check = send_at_command_and_check_response("AT+CGNSIPR=9600\r\n", "OK\r\n", rx_buffer);
+//  check = send_at_command_and_check_response("AT+CGNSIPR=9600\r\n", "OK\r\n", rx_buffer);
   check = send_at_command_and_check_response("AT+CGNSSEQ=\"RMC\"\r\n", "OK\r\n", rx_buffer);
   while (1)
   {
@@ -165,7 +165,6 @@ int main(void)
 	  strcpy(gp_buffer, rx_buffer);
 //	  replace_zeros_with_A(gp_buffer, sizeof(gp_buffer));
     /* USER CODE END WHILE */
-
 
     /* USER CODE BEGIN 3 */
 
@@ -278,7 +277,7 @@ static void MX_USART1_UART_Init(void)
 
   /* USER CODE END USART1_Init 1 */
   huart1.Instance = USART1;
-  huart1.Init.BaudRate = 115200;
+  huart1.Init.BaudRate = 9600;
   huart1.Init.WordLength = UART_WORDLENGTH_8B;
   huart1.Init.StopBits = UART_STOPBITS_1;
   huart1.Init.Parity = UART_PARITY_NONE;
